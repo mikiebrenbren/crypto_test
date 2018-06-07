24ff5c51046817f2d0592d8ea45233f370121021
Mike Brennan <mbrennan@sofi.org>
2018-06-07 17:11:04 -0600

    removing comments

--- profile/migration/run_psql.sh
+++ profile/migration/run_psql.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-#exec shmig -t postgresql -d kraken -l kraken -H localhost -p kraken -m /migrations "$1" "$2"
-#PGPASSWORD=kraken psql -h localhost -U kraken
 PGPASSWORD=kraken psql -h db -d kraken -U kraken  -p 5432 -a -q -f /usr/local/bin/init.sql
